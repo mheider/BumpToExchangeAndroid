ed7f05299afe4d259f0c2648d505c8da1de2a295
Markus H <markus.heider@bindoc.de>
2015-08-30 16:06:25 +0200

    register bump!

--- src/main/scala-2.11/de/hdrmrks/HelloScaloid.scala
+++ src/main/scala-2.11/de/hdrmrks/HelloScaloid.scala
@@ -1,16 +1,23 @@
 package de.hdrmrks
 
+import java.util.{Observable, Observer}
 import java.util.concurrent.{LinkedBlockingQueue, ThreadPoolExecutor, TimeUnit}
 
 import android.bluetooth.BluetoothDevice
+import android.content.Context
+import android.hardware.SensorManager
 import android.util.Log
 import org.scaloid.common._
+import bump._
 
 import scala.concurrent.ExecutionContext
 
-class HelloScaloid extends SActivity {
+class HelloScaloid extends SActivity with Observer {
+
+  var bumpDetector : BumpDetector = null
 
   val TAG = "HelloScaloid"
+
   implicit  val exec = ExecutionContext.fromExecutor(new ThreadPoolExecutor(100,100,1000,TimeUnit.SECONDS, new
       LinkedBlockingQueue[Runnable]))
 
@@ -25,6 +32,9 @@ class HelloScaloid extends SActivity {
 
   onCreate {
     Log.i(TAG, "Init play")
+    bumpDetector = new BumpDetector(getSystemService(Context.SENSOR_SERVICE)
+      .asInstanceOf[SensorManager])
+    bumpDetector.addBumpObserver(this)
     val devicesFuture = new BluetoothDiscoverer(getBaseContext).getBluetoothDevices
     devicesFuture.onSuccess {
       case result => {
@@ -37,5 +47,10 @@ class HelloScaloid extends SActivity {
         new BluetoothConnection(firstDevice)
       }
     }
+  }
+
+  override def update(observable: Observable, data: scala.Any): Unit = {
+    Log.i(TAG, "BUMP")
+    vibrator.vibrate(700)
   }
 }
